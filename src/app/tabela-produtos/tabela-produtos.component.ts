c806b56284e63b840accadcd309d383717291556
Luis Henrique Ries <prof.ries@gmail.com>
2021-10-28 20:58:39 -0300

    Add tabela de produtos e Input

--- src/app/tabela-produtos/tabela-produtos.component.ts
+++ src/app/tabela-produtos/tabela-produtos.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tabela-produtos',
+  selector: 'tabela-produtos',
   templateUrl: './tabela-produtos.component.html',
   styleUrls: ['./tabela-produtos.component.css']
 })
 export class TabelaProdutosComponent implements OnInit {
+  @Input('nome') nomeComponente = '';
+  produtos = [
+    {id:1, nome:"Prod1", preco:10},
+    {id:2, nome:"Prod2", preco:20},
+    {id:3, nome:"Prod3", preco:30},
+    {id:4, nome:"Prod4", preco:40},
+    {id:5, nome:"Prod5", preco:50},
+  ];
 
-  constructor() { }
+ constructor() { }
 
-  ngOnInit(): void {
-  }
+ ngOnInit(): void {
+ }
 
 }
+
